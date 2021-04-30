9a491432204c065b65e08800263cec2336137a48
Josh <josh.vigar@thebeansgroup.com>
2021-04-30 15:32:55 +0100

    Deal with duplicate users

--- api/controllers/coffee-controller.js
+++ api/controllers/coffee-controller.js
@@ -1,22 +1,27 @@
 const coffeePot = require('../models/coffee-pot');
 const { groupMessage } = require('../../lib/slack/coffee-bot');
 
-function index (req, res, next) {
+function addToPot(req, res, next) {
   const pot = coffeePot.find().then(
     pots => {
-      // TODO deal with duplicates
-      console.log(pots[0].beans_array.length)
-      pots[0].beans_array.push(req.body.user_id);
-      console.log(pots[0].beans_array.length)
-      if (pots[0].beans_array.length >= process.env.MAX_BEANS){
-        groupMessage(pots[0].beans_array.join(','))
+      const beansArray = pots[0].beans_array
+
+      if (beansArray.includes(req.body.user_id)){
+        res.send('You are already in the coffee-pot!');
+        return null;
+      }
+
+      beansArray.push(req.body.user_id);
+
+      if (beansArray.length >= process.env.MAX_BEANS){
+        groupMessage(beansArray.join(','))
         pots[0].beans_array = []
       }
+      
       pots[0].save();
-      console.log(pots[0]);
+      res.send('You have been added to the coffee-pot');
     }
   ).catch(next);
-  res.send('You have been added to the coffee-pot');
 };
 
-module.exports = { index };
+module.exports = { addToPot };
