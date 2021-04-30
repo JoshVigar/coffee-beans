13c6e3b88c2a63270294f435bbe5c6325ede2409
Josh <josh.vigar@thebeansgroup.com>
2021-04-30 15:16:04 +0100

    Add bot trigger to slash command

--- api/controllers/coffee-controller.js
+++ api/controllers/coffee-controller.js
@@ -1,10 +1,17 @@
 const coffeePot = require('../models/coffee-pot');
+const { groupMessage } = require('../../lib/slack/coffee-bot');
 
 function index (req, res, next) {
   const pot = coffeePot.find().then(
     pots => {
       // TODO deal with duplicates
+      console.log(pots[0].beans_array.length)
       pots[0].beans_array.push(req.body.user_id);
+      console.log(pots[0].beans_array.length)
+      if (pots[0].beans_array.length >= process.env.MAX_BEANS){
+        groupMessage(pots[0].beans_array.join(','))
+        pots[0].beans_array = []
+      }
       pots[0].save();
       console.log(pots[0]);
     }
